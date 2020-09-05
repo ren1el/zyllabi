aebe6dd9a2ca2637c804dd831a27531804a9783a
ren1el <renielxocampo@gmail.com>
2020-09-04 22:29:49 -0700

    Add delete functionality and delete/edit modals.

---
+++ src/components/EditModal.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import quarters from '../utils/quarters';
+import years from '../utils/years';
+import { Button, Modal, Form } from 'react-bootstrap';
+
+const EditModal = ({ syllabus }) => {
+  const [show, setShow] = useState(false);
+  const [instructor, setInstructor] = useState(syllabus.instructor);
+  const [quarter, setQuarter] = useState(syllabus.quarter);
+  const [year, setYear] = useState(syllabus.year);
+
+  const handleShow = () => setShow(true);
+
+  const handleClose = () => setShow(false);
+
+  const handleSave = () => {
+    console.log('Saving...');
+    handleClose();
+  };
+
+  return (
+    <>
+      <Button className="m-1" variant="primary" onClick={handleShow}>
+        Edit
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit Syllabus for {syllabus.course.department.name} {syllabus.course.courseNumber}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="instructor">
+              <Form.Label>Instructor</Form.Label>
+              <Form.Control type="text" value={instructor} onChange={({ target }) => setInstructor(target.value)}/>
+            </Form.Group>
+
+            <Form.Group controlId="quarter">
+              <Form.Label>Quarter</Form.Label>
+              <Form.Control as="select" value={quarter} onChange={({ target }) => setQuarter(target.value)}>
+                {quarters.map((savedQuarter) => <option key={savedQuarter} value={savedQuarter}>{savedQuarter}</option>)}
+              </Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="year">
+              <Form.Label>Year</Form.Label>
+              <Form.Control as="select" value={year} onChange={({ target }) => setYear(target.value)}>
+                {years.map((savedYear) => <option key={savedYear} value={savedYear}>{savedYear}</option>)}
+              </Form.Control>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleSave}>
+            Save
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default EditModal;
