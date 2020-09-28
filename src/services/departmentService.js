fea43b149170dc0cb203264630e869b92c126110
ren1el <renielxocampo@gmail.com>
2020-09-27 20:58:53 -0700

    Change API url when in development mode

--- src/services/departmentService.js
+++ src/services/departmentService.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
-const baseUrl = `/api/departments`; //eslint-disable-line
+let baseUrl = '/api/departments';
+
+if (process.env.NODE_ENV === 'development') { //eslint-disable-line
+  baseUrl = 'http://localhost:3001/api/departments';
+} 
 
 const getAllDepartments = async () => {
   const response = await axios.get(baseUrl);
--- src/services/usersService.js
+++ src/services/usersService.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
-const baseUrl = `/api/user`; //eslint-disable-line
+let baseUrl = '/api/user';
+
+if (process.env.NODE_ENV === 'development') { //eslint-disable-line
+  baseUrl = 'http://localhost:3001/api/user';
+} 
 
 const getUserContributions = async (googleId) => {
   const response = await axios.get(`${baseUrl}/${googleId}`);
